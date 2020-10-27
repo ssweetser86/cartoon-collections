46f449aa3f7df165fd698697e539dde39376d7f0
Stephen <stephenjsweets@gmail.com>
2020-10-27 19:23:47 -0400

    commit tree d1d21ba1049b02cbbd058e94b92b8d90d417f5b8

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |value, index|
+    puts "#{index}. #{value}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map { |value| value.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  return true if array.find { |value| value.length > 4 }
+  return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  i = 0
+  while i < array.length
+    return array[i] if cheese_types.include?(array[i])
+    i += 1
+  end
+  return nil
 end
